2ce7a5478dd85bc04ad9e36f1b8cb30afeefb9fc
pvdu2003 <152840528+pvdu2003@users.noreply.github.com>
2024-07-03 10:09:48 +0700

    Add pagination in category detail

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -2,12 +2,25 @@ const Category = require("../models/category.schema");
 const Book = require("../models/book.schema");
 
 class categoryController {
-  // [GET] /category/:id
+  // [GET] /category/:id?page
   async getById(req, res, next) {
     let id = req.params.id;
     const category = await Category.findById(id);
-    const books = await Book.find({ cat_id: id });
-    res.render("pages/category/categoryDetail", { books, category });
+    let currentPage = parseInt(req.query.page) || 1;
+    const limit = 10;
+    const totalBooks = await Book.countDocuments({
+      cat_id: id,
+    });
+    const totalPages = Math.ceil(totalBooks / limit);
+    const books = await Book.find({ cat_id: id })
+      .skip((currentPage - 1) * limit)
+      .limit(limit);
+    res.render("pages/category/categoryDetail", {
+      books,
+      category,
+      currentPage,
+      totalPages,
+    });
   }
 }
 
