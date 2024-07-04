60f7d5d2637e92fd9b2ff33e5da9548103d13aab
pvdu2003 <152840528+pvdu2003@users.noreply.github.com>
2024-07-04 17:09:47 +0700

    Add book list page

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -8,6 +8,25 @@ class bookController {
     const book = await Book.findById(id);
     res.render("pages/book/bookDetail", { book });
   }
+
+  // [GET] /book/list
+  async getAll(req, res, next) {
+    try {
+      const currentPage = parseInt(req.query.page) || 1;
+      const size = parseInt(req.query.size) || 20;
+
+      const books = await Book.find()
+        .skip((currentPage - 1) * size)
+        .limit(size);
+      const totalBooks = await Book.countDocuments();
+      const totalPages = Math.ceil(totalBooks / size);
+
+      res.render("pages/book/bookList", { books, currentPage, totalPages });
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).send("An error occurred while fetching books.");
+    }
+  }
 }
 
 module.exports = new bookController();
--- routes/book.route.js
+++ routes/book.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bookController = require("../controllers/book.controller");
+router.get("/list", bookController.getAll);
 router.get("/:id", bookController.getById);
 
 module.exports = router;
