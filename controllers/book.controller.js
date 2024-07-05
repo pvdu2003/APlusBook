df95090eaf53860e228f097bf58015b9bae22a0e
pvdu2003 <152840528+pvdu2003@users.noreply.github.com>
2024-07-05 09:20:24 +0700

    Add query about price and title in book list

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -9,19 +9,63 @@ class bookController {
     res.render("pages/book/bookDetail", { book });
   }
 
-  // [GET] /book/list
+  // [GET] /book/list?page&from&to&title
   async getAll(req, res, next) {
     try {
       const currentPage = parseInt(req.query.page) || 1;
       const size = parseInt(req.query.size) || 20;
-
-      const books = await Book.find()
-        .skip((currentPage - 1) * size)
-        .limit(size);
-      const totalBooks = await Book.countDocuments();
+      const from = parseInt(req.query.from) || 0;
+      const to = parseInt(req.query.to) || 150;
+      const title = req.query.title || "";
+      let books;
+      let totalBooks;
+      // Only range of price
+      if (from > 0 && to > 0) {
+        books = await Book.find({
+          price: { $gte: from, $lte: to },
+        })
+          .skip((currentPage - 1) * size)
+          .limit(size);
+        totalBooks = await Book.countDocuments({
+          price: { $gte: from, $lte: to },
+        });
+        // Only title
+      } else if (title) {
+        books = await Book.find({ title: { $regex: title, $options: "i" } })
+          .skip((currentPage - 1) * size)
+          .limit(size);
+        totalBooks = await Book.countDocuments({
+          title: { $regex: title, $options: "i" },
+        });
+        // Both price and title
+      } else if (from > 0 && to > 0 && title) {
+        books = await Book.find({
+          price: { $gte: from, $lte: to },
+          title: { $regex: title, $options: "i" },
+        })
+          .skip((currentPage - 1) * size)
+          .limit(size);
+        totalBooks = await Book.countDocuments({
+          price: { $gte: from, $lte: to },
+          title: { $regex: title, $options: "i" },
+        });
+        // All books
+      } else {
+        books = await Book.find()
+          .skip((currentPage - 1) * size)
+          .limit(size);
+        totalBooks = await Book.countDocuments();
+      }
+      console.log("Total: " + totalBooks);
       const totalPages = Math.ceil(totalBooks / size);
-
-      res.render("pages/book/bookList", { books, currentPage, totalPages });
+      res.render("pages/book/bookList", {
+        books,
+        currentPage,
+        totalPages,
+        from,
+        to,
+        title,
+      });
     } catch (error) {
       console.error("Error:", error);
       res.status(500).send("An error occurred while fetching books.");
--- routes/book.route.js
+++ routes/book.route.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bookController = require("../controllers/book.controller");
-router.get("/list", bookController.getAll);
-router.get("/:id", bookController.getById);
+const authMiddleware = require("../middlewares/auth.middleware");
+router.get("/list", authMiddleware, bookController.getAll);
+router.get("/:id", authMiddleware, bookController.getById);
 
 module.exports = router;
